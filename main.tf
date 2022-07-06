beb47a8301cfcaa4487e9747c915b925f38bb406
Daniel Sol <>
2022-07-05 18:54:14 -0700

    simplification

--- main.tf
+++ main.tf
@@ -11,28 +11,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.region 
 
 }
-
-resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
-  location            = var.region
-  resource_group_name = var.rg_name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
-
-  default_node_pool {
-    name            = "default"
-    node_count      = 3
-    vm_size         = "Standard_D2_v2"
-    os_disk_size_gb = 30
-  }
-
-  service_principal {
-    client_id     = var.k8s_spn_client_id
-    client_secret = var.k8s_spn_client_secret
-  }
-
-  role_based_access_control_enabled = true
-
-  tags = {
-    environment = "Demo"
-  }
-}
--- variables.tf
+++ variables.tf
@@ -1,19 +1,10 @@
 variable "region" {
   description = "Azure region"
-  default     = "westus2"
+  default     = "eastus"
 }
 
 variable "rg_name" {
   description = "Resource Group name"
   default     = "tfOperatorRg"
 }
-
-variable "k8s_spn_client_id" {
-  default     = "updatePerK8sCluster"
-}
-
-variable "k8s_spn_client_secret" {
-  default     = "updatePerK8sCluster"
-}
-
 
