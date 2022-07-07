29fcc761138586336829473fa7e5f409bcad78a9
Daniel Sol <>
2022-07-07 16:13:18 -0700

    creating vm conf

---
+++ eks/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-098e42ae54c764c35"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tfTest"
+  }
+}
+
