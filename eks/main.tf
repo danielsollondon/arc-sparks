bff4b5ba2abf16c44d855cde7687941b4211d5c7
Daniel Sol <>
2022-07-07 16:37:14 -0700

    adding in cred vars

--- eks/main.tf
+++ eks/main.tf
@@ -1,14 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-}
+
+variable "access_key" { }
+variable "secret_key" { }
 
 provider "aws" {
   region  = "us-west-2"
+access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "app_server" {
@@ -16,7 +13,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "tfTest"
+    Name = "testTf"
   }
-}
+
 
