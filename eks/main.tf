bcd7f97c903886267fc2937f120f7effd491a5bf
Daniel Sol <>
2022-07-11 08:49:54 -0700

    removing vars in main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -1,11 +1,5 @@
-
-variable "access_key" { }
-variable "secret_key" { }
-
 provider "aws" {
   region  = "us-west-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 resource "aws_instance" "app_server" {
